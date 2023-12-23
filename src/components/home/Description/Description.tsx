59fd3533d90654c3f8e74fb97974e6d7ea6dccb0
Francisco Cervantes <fjccandelario@gmail.com>
2023-12-23 06:05:17 -0600

    Se agregan estilos al componente de Description

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,6 +1,8 @@
+import styles from "./Description.module.sass"
+
 export const Description = () => {
     return (
-        <section>
+        <section className={styles.Description}>
             <img src="/images/description.jpeg" alt="Products marketplace" />
             <h2>Description</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus perspiciatis aperiam dolore consequuntur nulla ducimus ab, iusto illum exercitationem laborum non nihil, quas, amet accusamus reiciendis laudantium autem culpa?</p>
