8cb49dbd1d348a590d37467316c2c158565be2d0
Francisco Cervantes <fjccandelario@gmail.com>
2023-12-23 05:46:55 -0600

    Se agregan archivos estaticos, componente description

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,9 @@
 export const Description = () => {
     return (
         <section>
-            <h1>Description</h1>
+            <img src="/images/description.jpeg" alt="Products marketplace" />
+            <h2>Description</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus perspiciatis aperiam dolore consequuntur nulla ducimus ab, iusto illum exercitationem laborum non nihil, quas, amet accusamus reiciendis laudantium autem culpa?</p>
         </section>
     )
 }
