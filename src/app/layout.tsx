63a1d04d8413f38899ba85286e993c0d70b657ab
Francisco Cervantes <fjccandelario@gmail.com>
2023-12-23 05:36:41 -0600

    Se acomoda la estrucutra de components y sass, se utiliza importación corta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-import { Header } from './components/shared/Header'
-import { Footer } from './components/shared/Footer'
+import { Header } from 'app/components/shared/Header'
+import { Footer } from 'app/components/shared/Footer'
+import 'app/sass/globals.sass'
 
 const inter = Inter({ subsets: ['latin'] })
 
