e7ca907469e6bbf286e48697ffca139d6143924d
Francisco Cervantes <fjccandelario@gmail.com>
2023-12-26 02:42:51 -0600

    Se agrega el loaging al inicio, se muestran productos en la tienda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 //import { Inter } from 'next/font/google'
 import { Roboto } from 'next/font/google'
 import { Header } from 'app/components/shared/Header'
+import { Description } from "app/components/home/Description"
+import { Hero } from "app/components/home/Hero"
 import { Footer } from 'app/components/shared/Footer'
 import 'app/sass/globals.sass'
 
@@ -24,6 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Header/>
+        <Hero />
+        <Description />
         {children}
         <Footer />
       </body>
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,14 +1,21 @@
+import Image from 'next/image'
+import styles from './MainProducts.module.sass'
 
 const getProducts = async () => {
-    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,{
-        headers: new Headers({
-            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+    
+    try {
+        const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,{
+            headers: new Headers({
+                'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+            })
         })
-    })
-
-    const data = await response.json()
-
-    return data
+    
+        const { products } = await response.json()
+    
+        return products
+    } catch (error) {
+       console.log(error) 
+    }    
 }
 
 
@@ -17,10 +24,20 @@ export const MainProducts = async () => {
     
     const products = await getProducts()
 
-    console.log(products)
     return (
-        <section>
-            <h1>MainProductss</h1>
+        <section className={styles.MainProducts}>
+            <h3>✨ New Products released!</h3>
+            <div className={styles.MainProducts__grid}>
+                {products?.map((product) =>{
+                    const imageSrc = product.images[0].src;
+                    return (
+                        <article>
+                            <p>{ product.title }</p>
+                            <Image src={imageSrc} fill alt={product.title} loading='eager'/>
+                        </article>
+                    )
+                })}
+            </div>
         </section>
     )
 }
