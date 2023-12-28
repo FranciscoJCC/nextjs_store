f36d2fb5111c6c80bd2dbc392df42b020fdaa544
Francisco Cervantes <fjccandelario@gmail.com>
2023-12-28 01:48:20 -0600

    Se configura página de error, sen sección de productos

---
+++ src/app/(home)/error.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useEffect } from "react";
+
+interface ErrorProps {
+    error: Error;
+    reset: () =>void
+}
+
+export default function Error({error, reset}: ErrorProps){
+
+    useEffect(() => {
+        //Se recomienda utilizar un software de observabilidad para mandar los errores
+        console.log(error)
+    }, [])
+
+    return(
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            minHeight: "100vh",
+          }}>
+            <h1> :( </h1>
+            <p>Ha ocurrido un error</p>
+            <button onClick={reset}>Intentar de nuevo</button>
+        </div>        
+    )
+}
