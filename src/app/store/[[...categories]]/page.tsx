2c5a78e82dd84d6ce41641b9b53f9150aeb976f9
Francisco Cervantes <fjccandelario@gmail.com>
2024-01-02 01:46:07 -0600

    Se agregan filtros en la sección de store

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,18 +1,28 @@
 import { ProductsWrapper } from "app/components/store/ProductsWrapper"
+import { getCollections, getCollectionsProducts } from "app/services/shopify/collections"
 import { getProducts } from "app/services/shopify/products"
 
 interface CategoryProps {
     params: {
         categories: string[],
-        searchParams? : string
     }
+    searchParams? : string
 }
 
 export default async function Category(props: CategoryProps) {
-
-    const products = await getProducts()    
+    //parametros de ruta
     const { categories } = props.params
 
+    let products = []
+    const collections = await getCollections()
+
+    if(categories?.length > 0){
+        const selectedCollectionId = collections.find((collection : any) => collection.handle === categories[0]).id
+        products = await getCollectionsProducts(selectedCollectionId)
+    }else{
+        products = await getProducts()
+    }
+    
     return(
         <ProductsWrapper products={products}/>
     )
