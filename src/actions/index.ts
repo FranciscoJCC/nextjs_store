4f9c880a6c3ed9e7571d30c05563481706365d8d
Francisco Cervantes <fjccandelario@gmail.com>
2024-01-03 03:10:45 -0600

    Se agrega funcion para registrar clientes desde api

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,21 @@
 "use server"
 
-export const handleCreateUser = (formData : any) => {
-    console.log("handle", formData)
+import { GraphQLClientSingleton } from "app/graphql"
+import { createUserMutation } from "app/graphql/mutations/createUserMutation"
+
+export const handleCreateUser = async (formData : FormData) => {
+    const formDataObject = Object.fromEntries(formData);
+    delete formDataObject["password_confirmation"]; // Lo eliminamos por que ya validamos que sean iguales los passwords
+    const graphQLClient = GraphQLClientSingleton.getInstance().getClient();
+
+    const variables = {
+        input: {
+            ...formDataObject,
+            phone: '+52' + formDataObject.phone
+        }
+    }
+
+
+    const data = await graphQLClient.request(createUserMutation, variables)
+    console.log(data);
 }
---
+++ src/graphql/mutations/customerAccessTokenCreate.ts
@@ -0,0 +1,15 @@
+import { gql } from "graphql-request";
+
+export const customerAccessTokenCreaeMutation = gql`
+    mutation customerAccessTokenCreate ($email: String!, $password: String){
+        customerAccessTokenCreate(input: {email: $email, password: $password}){
+            customerAccessToken {
+                accessToken
+                expiresAt
+            }
+            customerUserErrors {
+                message
+            }
+        }
+    }
+`
