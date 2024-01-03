18104b34c1f39256c0fc4a63956a31b414bb98db
Francisco Cervantes <fjccandelario@gmail.com>
2024-01-02 23:14:45 -0600

    Se agrega ceo para pagina estatica en home y dinamica en product - handle

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
 import { MainProducts } from "app/components/home/MainProducts"
+import { Metadata } from "next"
 
+export const metadata: Metadata = {
+  title: "✨ Future World",
+  description: "Welcomen to the future world, an ecommerce from other currency",
+  keywords: ["ecommerce", "future", "world"]
+}
 
 export default function Home() {
   return (
