82082018505a1cd84124db0c01002c07ed95e411
Francisco Cervantes <fjccandelario@gmail.com>
2024-01-08 23:02:24 -0600

    Se agrega agente para dar recomendaciones

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,23 @@
 import { Chat } from "app/components/chat/chat"
+import { getProducts } from "app/services/shopify/products"
+import { createAgent } from "app/utils/openai/createAgent"
 
-export default function ChatPage() {
+export default async function ChatPage() {
+
+    const products = await getProducts()
+    const productTiles = products.map((product) => product.title)
+    const flatProductTitles = productTiles.join("\n");
+
+    const agent = createAgent(flatProductTitles);
+
+
+    console.log(flatProductTitles);
+
+
     return (
         <>
             <h1>Chatbot</h1>
-            <Chat />
+            <Chat agent={agent}/>
         </>
     )
 }
